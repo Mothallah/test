3b941f061b9b4f1ac9bd465d6775fabbb21c2bc3
Mothallah <vladimirmakeev121@gmail.com>
2018-12-03 22:20:12 +0300

    Main.java was update

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int number = scan.nextInt();
         System.out.println("Mothallah love this project");
-        System.out.printf("Look, Mothallah enter this digits: %d", number);
+        System.out.printf("Look, Mothallah enter this digits: %d\n", number);
+        for(int i = 0; i < 10; i++){
+            System.out.printf("Now, this is %d\n", i);
+        }
     }
 }
