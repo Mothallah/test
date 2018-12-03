b05612285f3976a51fbb32b9eeaff35b73235891
Mothallah <vladimirmakeev121@gmail.com>
2018-12-03 22:04:39 +0300

    Main.java input of int was added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
         System.out.println("Mothallah love this project");
+        System.out.printf("Look, Mothallah enter this digits: %d", number);
     }
 }
